f9d3f792a45d160d975970eee943e38f6ad40f59
CevicheConAji <zavalachirapiero@gmail.com>
2024-12-10 19:26:58 +0100

    Refactor connection initialization in Main class.
    Moved the database connection initialization closer to its usage for improved code readability and logical flow. Removed an unused import to clean up the code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 
 import org.example.Controller.*;
-import org.example.DB.ConnectDB;
 
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
         ControllerDB controllerDB = new ControllerDB();
-        Connection con = controllerDB.connectDB();
-
         ControllerCity controllerCity = new ControllerCity();
         ControllerCountry controllerCountry = new ControllerCountry();
         ControllerFilm controllerFilm = new ControllerFilm();
         ControllerActor controllerActor = new ControllerActor();
+
+        Connection con = controllerDB.connectDB();
 
 
         controllerCity.showCityInfo(controllerCity.getOneHundredCities(con));
